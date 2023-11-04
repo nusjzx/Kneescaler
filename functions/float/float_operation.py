cf711d100884a24975dd0d92f7e5b38ab16fa7e1
nusjzx <nujzx@gmail.com>
2023-11-05 03:12:50 +0800

    add metrics exporter

--- functions/float/float_operation.py
+++ functions/float/float_operation.py
@@ -1,8 +1,18 @@
 from flask import Flask, request, jsonify
+from prometheus_flask_exporter import PrometheusMetrics
+
 
 app = Flask(__name__)
+metrics = PrometheusMetrics(app)
+
+
+by_path_counter = metrics.counter(
+    'by_path_counter', 'Request count by request paths',
+    labels={'status': lambda r: r.status_code, 'path': lambda r: request.path}
+)
 
 @app.route('/float_operation', methods=['GET'])
+@by_path_counter
 def float_operation():
     try:
         result = 0.0
@@ -11,6 +21,7 @@ def float_operation():
         return jsonify({"result": result})
     except Exception as e:
         return jsonify({"error": str(e)})
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
