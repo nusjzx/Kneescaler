0667f68ffa30f2c5ef31cfad31b1272c4ecc10ea
Soumya Ranjan Sahoo <54215971+animosoumya@users.noreply.github.com>
2023-11-04 10:35:55 +0000

    Update matmul.py to be exposed as endpoints to call

--- functions/matmul/matmul.py
+++ functions/matmul/matmul.py
@@ -1,13 +1,21 @@
 import numpy as np
+from flask import Flask, jsonify
 
-def matmul(size):
-    
-    matrix_A = np.random.rand(size, size)
-    matrix_B = np.random.rand(size, size)
+app = Flask(__name)
+
+fixed_size = 100  # Fixed size for the matrix
+
+@app.route('/matmul', methods=['POST'])
+def matmul():
+    matrix_A = np.random.rand(fixed_size, fixed_size)
+    matrix_B = np.random.rand(fixed_size, fixed_size)
 
     result_matrix = np.dot(matrix_A, matrix_B)
 
-    return result_matrix
+    return jsonify({"result_matrix": result_matrix.tolist()})
 
-# Example usage:
-# result = matmul(100)  # Perform matrix multiplication with 100x100 matrices
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
+
+
+# We can POST request to http://<our-service-ip>:8080/matmul
