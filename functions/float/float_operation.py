c234e3576c3296fee777b1732400ef6f8e877be2
Soumya Ranjan Sahoo <54215971+animosoumya@users.noreply.github.com>
2023-11-04 10:18:16 +0000

    Update float_operation.py to be exposed as endpoints to call

--- functions/float/float_operation.py
+++ functions/float/float_operation.py
@@ -1,5 +1,20 @@
-def float_operation(event, context):
-    result = 0.0
-    for i in range(1000000):
-        result += i / 3.14159265359
-    return {"result": result}
+from flask import Flask, request, jsonify
+
+app = Flask(__name)
+
+@app.route('/float_operation', methods=['POST'])
+def float_operation():
+    try:
+        data = request.get_json()
+        result = 0.0
+        for i in range(1000000):
+            result += i / 3.14159265359
+        return jsonify({"result": result})
+    except Exception as e:
+        return jsonify({"error": str(e)})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
+
+# Once it's deployed, we can make POST requests to http://<your-service-ip>:8080/float_operation 
+# to invoke your function and get the result.
